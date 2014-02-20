a1cc2f47f76d8b7d97fbc87e61f388173fd11fdc
afleisch <afleisch@gmail.com>
2014-02-19 21:39:44 -0800

    added routes for update and destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ SpaApp::Application.routes.draw do
   root to: 'todos#index'
   get '/todos', to: 'todos#index', as: 'todos'
   post '/todos', to: 'todos#create'
+  delete '/todos/:id', to: 'todos#destroy'
+  patch '/todos/id', to: 'todos#update'
+  
 end
