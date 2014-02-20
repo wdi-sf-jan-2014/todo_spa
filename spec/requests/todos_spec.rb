2a0a28eb1bc6bc8c9e239e125cffe3c877d95081
smarsh25 <sjm.atl@gmail.com>
2014-02-19 19:19:52 -0800

    correcting params in update spec

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -53,7 +53,7 @@ describe "Todos" do
     before do
       @todo = Todo.create!(title: "test todo", completed: false)
     end
-    let(:data) { {completed: true} }
+    let(:data) { {todo: {completed: true}} }
     it "should allow the caller to update the completed attribute on the todo" do
       patch "/todos/#{@todo.id}.json", data
       response.status.should == 200
