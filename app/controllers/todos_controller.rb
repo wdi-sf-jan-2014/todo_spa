d304777ab486d8f0c334d9d52b405fcf15d849e5
nathantross <nathantross@gmail.com>
2014-02-19 19:07:04 -0800

    added delete

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -18,9 +18,34 @@ class TodosController < ApplicationController
 
   # Fill in destroy
   def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+
+    respond_to do |f|
+      f.html { redirect_to todos_path }
+      f.json { render :json => {}, status: 200}
+    end
   end
 
   # Fill in update
   def update
+    todo_params = params.permit(:title, :completed)
+      @todo = Todo.create(todo_params)
+
+      respond_to do |f|
+      f.html
+      f.json { render :json => @todo, only: [:id, :title, :completed] }
+
+    end
   end
 end
+
+
+
+    # url = params.require(:site)[:url]
+    # @site = Site.create(url: url)
+    # LinksWorker.perform_async(@site.id)
+    # respond_to do |f|
+    #   f.html { redirect_to site_path(@site) }
+    #   f.json { render :json => @site }
+    # end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@ SpaApp::Application.routes.draw do
   root to: 'todos#index'
   get '/todos', to: 'todos#index', as: 'todos'
   post '/todos', to: 'todos#create'
+  delete '/todos/:id', to: 'todos#destroy'
+  patch '/todos/:id', to: 'todos#update'
+
 end
+
+
+
+  ## root 'sites#index'
+  ## get '/sites/new', to: 'sites#new', as: 'new_site'
+  # post '/sites', to: 'sites#create', as: 'sites'
+  # get '/sites/:id', to: 'sites#show', as: 'site'
+  # delete '/sites/:id', to: 'sites#destroy'
+  # get '/sites', to: 'sites#index'
