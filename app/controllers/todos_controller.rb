adc1c65bb251337c7cbb18d0d63f34e136a4d1d7
David Simons <dave.c.simons@gmail.com>
2014-02-20 09:09:15 -0800

    completed jquery todo spa app

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -18,9 +18,22 @@ class TodosController < ApplicationController
 
   # Fill in destroy
   def destroy
+    todo = Todo.find(params[:id])
+    todo.destroy
+
+    respond_to do |f|
+      f.json { render :json => todo, only: [:id, :title, :completed] }
+    end
   end
 
   # Fill in update
   def update
+    new_todo = params.require(:todo).permit(:title, :completed)
+    todo = Todo.find(params[:id])
+    todo.update_attributes(new_todo)
+
+    respond_to do |f|
+      f.json { render :json => todo, only: [:id, :title, :completed] }
+    end
   end
 end
