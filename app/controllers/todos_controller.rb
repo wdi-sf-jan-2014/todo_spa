2946f9bd5b781249cbe26f407948602b5d6cdb76
JSchulhof <JSchulhof@gmail.com>
2014-02-19 17:48:43 -0800

    working delete and update

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,6 @@
 class TodosController < ApplicationController
   def index
-    @todos = Todo.all
+    @todos = Todo.all.sort
     respond_to do |f|
       f.html
       f.json { render :json => @todos, only: [:id, :title, :completed]}
@@ -18,9 +18,25 @@ class TodosController < ApplicationController
 
   # Fill in destroy
   def destroy
+      id = params[:id]
+      todo = Todo.find(id)
+      todo.delete
+      
+    respond_to do |f|
+      f.html {redirect_to root_path}
+      f.json { render :json => todo, only: [:id, :title, :completed]}
+    end
   end
 
   # Fill in update
   def update
+    @todo = Todo.find(params[:id])
+    todo_params = params.require(:todo).permit(:title, :completed)
+    @todo.update_attributes(todo_params)
+
+    respond_to do |f|
+      f.html
+      f.json { render :json => @todo, only: [:id, :title, :completed]}
+    end
   end
 end
