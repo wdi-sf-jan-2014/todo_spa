7327ac48c139869f85a16c91415414d24092e30c
kaifius <kai.hofius@gmail.com>
2014-03-03 14:42:46 -0800

    add todo works, but test doesn't pass

--- app/assets/javascripts/views/todos/todos_index.js
+++ app/assets/javascripts/views/todos/todos_index.js
@@ -3,6 +3,28 @@ SpaApp.Views.TodosIndex = Backbone.View.extend({
 
   template: HandlebarsTemplates['todos/index'],
 
+  events: {
+    'submit #addTodo': 'newTodo',
+    'click #todo_completed': 'editTodo',
+    'click #removeTodo': 'deleteTodo'
+  },
+
+  newTodo: function(){
+    event.preventDefault();
+    data = { title: $("#todo_title").val() };
+
+    $.ajax({
+      type: 'post',
+      url: '/todos.json',
+      data: { todo: data },
+      context: this
+    }).done(function(response){
+      console.log(response);
+      var todoHTML = HandlebarsTemplates['todos/show'](response);
+      this.$el.append(todoHTML);
+    });
+  },
+
   render: function() {
     $(this.el).html(this.template());
 
