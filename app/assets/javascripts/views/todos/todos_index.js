a5afc21a41d0b6f4c698f68ba81b9448122e4d55
ste claudino daffara <ste.daff@gmail.com>
2014-03-03 12:26:59 -0800

    commented todos_index.js

--- app/assets/javascripts/views/todos/todos_index.js
+++ app/assets/javascripts/views/todos/todos_index.js
@@ -4,14 +4,19 @@ SpaApp.Views.TodosIndex = Backbone.View.extend({
   template: HandlebarsTemplates['todos/index'],
 
   render: function() {
+    // renders the add_form template and appends to our view
     $(this.el).html(this.template());
 
+    // looping through each element in our collection
     _.each(this.collection, function (someTodo) {
       var todoHTML = HandlebarsTemplates['todos/show'](someTodo);
       this.$el.append(todoHTML);
-    }, this);
+    }, this); //final "this" defines the scope of the function
 
     return this;
   }
 
 });
+
+
+// @collection.each do |someTodo|
