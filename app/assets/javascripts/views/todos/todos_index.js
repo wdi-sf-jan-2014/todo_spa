fd5e63db51a3e060e43a369ea5872836f1fb77b9
elidarrow <elidarrow@gmail.com>
2014-03-03 13:15:05 -0800

    committing morning work

--- app/assets/javascripts/views/todos/todos_index.js
+++ app/assets/javascripts/views/todos/todos_index.js
@@ -3,6 +3,29 @@ SpaApp.Views.TodosIndex = Backbone.View.extend({
 
   template: HandlebarsTemplates['todos/index'],
 
+  events: {
+    "submit #addTodo": "create"
+  },
+
+  create: function(event) {
+    event.preventDefault();
+    console.log("New Todo submitted");
+    var newTodo = {
+      title: $("#todo_title").val(),
+      completed: false
+    };
+    console.log(newTodo);
+    var _this = this;
+    $.post('/todos.json', {
+      todo: newTodo
+    }).done(function (responseData) {
+      console.log(responseData);
+      var todoHTML = HandlebarsTemplates['todos/show'](responseData);
+      _this.$el.append(todoHTML);
+    });  
+
+  },
+
   render: function() {
     $(this.el).html(this.template());
 
