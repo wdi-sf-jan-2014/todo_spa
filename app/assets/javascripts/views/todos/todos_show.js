f9e644e523ff254483092ecd89e97b38a13669d1
ste claudino daffara <ste.daff@gmail.com>
2014-03-03 19:12:45 -0800

    delete method works

---
+++ app/assets/javascripts/views/todos/todos_show.js
@@ -0,0 +1,54 @@
+SpaApp.Views.TodosShow = Backbone.View.extend({
+  class: 'todo',
+
+  template: HandlebarsTemplates['todos/show'],
+
+  events: {
+    'click #todo_completed': 'toggleComplete',
+    'click #removeTodo': 'deleteTodo'
+  },
+
+  render: function() {
+
+    $(this.el).html(this.template(this.model));
+
+    return this;
+
+  },
+
+  toggleComplete: function()  {
+    var _this = this;
+
+    if(this.model.completed){
+      this.model.completed = false;
+    }else{
+      this.model.completed = true;
+    }
+
+    $.ajax({
+      url: '/todos/'+this.model.id+ '.json',
+      type: 'patch',
+      data: { todo: this.model},
+      context: this
+    }).done(function (data) {
+      $(this.el).toggleClass("done-true");
+    });
+
+  },
+
+  deleteTodo: function()  {
+
+    $.ajax({
+      type: 'delete',
+      url: '/todos/'+this.model.id+'.json',
+      context: this,
+      data: { todo: this.model}
+    })
+      .done(function (data) {
+        $(this.el).remove();
+      });
+
+  }
+
+
+});
