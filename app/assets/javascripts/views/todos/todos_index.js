01b6cb84b4b69c016ad607150fc3041c6794eb65
kaifius <kai.hofius@gmail.com>
2014-03-03 17:21:33 -0800

    add, update, delete all work

--- app/assets/javascripts/views/todos/todos_index.js
+++ app/assets/javascripts/views/todos/todos_index.js
@@ -4,36 +4,36 @@ SpaApp.Views.TodosIndex = Backbone.View.extend({
   template: HandlebarsTemplates['todos/index'],
 
   events: {
-    'submit #addTodo': 'newTodo',
-    'click #todo_completed': 'editTodo',
-    'click #removeTodo': 'deleteTodo'
-  },
-
-  newTodo: function(){
-    event.preventDefault();
-    data = { title: $("#todo_title").val() };
-
-    $.ajax({
-      type: 'post',
-      url: '/todos.json',
-      data: { todo: data },
-      context: this
-    }).done(function(response){
-      console.log(response);
-      var todoHTML = HandlebarsTemplates['todos/show'](response);
-      this.$el.append(todoHTML);
-    });
+    'submit #addTodo': 'addTodo'
   },
 
   render: function() {
     $(this.el).html(this.template());
 
+    var todoView;
     _.each(this.collection, function (someTodo) {
-      var todoHTML = HandlebarsTemplates['todos/show'](someTodo);
-      this.$el.append(todoHTML);
+      todoView = new SpaApp.Views.TodosShow({ model: someTodo });
+      this.$el.append(todoView.render().el);
     }, this);
 
     return this;
+  },
+
+  addTodo: function(event){
+    event.preventDefault();
+    var data = { title: $("#todo_title").val(), completed: false };
+    // var _this = this;
+
+    $.ajax({
+      type: 'POST',
+      url: '/todos.json',
+      data: { todo: data },
+      context: this
+    }).done(function(response){
+      console.log(response);
+      var todoView = new SpaApp.Views.TodosShow({ model: response});
+      this.$el.append(todoView.render().el);
+    });
   }
 
 });
