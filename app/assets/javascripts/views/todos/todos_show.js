effc060795eb4452e03da16cee116a490d2a8d04
smarsh25 <sjm.atl@gmail.com>
2014-03-04 12:13:18 -0800

    adding complete checkbox

--- app/assets/javascripts/views/todos/todos_show.js
+++ app/assets/javascripts/views/todos/todos_show.js
@@ -4,27 +4,51 @@ SpaApp.Views.TodosShow = Backbone.View.extend({
   template: HandlebarsTemplates['todos/show'],
 
    events: {
-  //   'click input[type="checkbox"]': 'complete',
-     'click #removeTodo': 'removeTodo'
+    'click input[type="checkbox"]': 'complete',
+    'click #removeTodo': 'removeTodo'
    },
 
   render: function() {
-    $(this.el).html(this.template(this.model));
+    if (this.model.completed) {
+      this.$el.addClass("done-true");
+    }
+    this.$el.html(this.template(this.model));
 
     return this;
   },
 
-  removeTodo: function() {
-    _this = this;
+  removeTodo: function(event) {
+    event.preventDefault();
+    var _this = this;
     var id = this.model.id;
 
     $.ajax({
       type: 'delete',
       url: '/todos/' + id
-    })
-      .done(function (data) {
-        _this.$el.remove();
-      });
+    }).done(function(data) {
+      _this.$el.remove();
+    });
+  },
+
+  complete: function() {
+    var _this = this;
+    var id = this.model.id;
+    // toggle model value, then collect updated info to send 
+    this.model.completed = !this.model.completed;
+    var updatedTodo = {
+      completed: this.model.completed
+    };
+
+    // issue update request to server
+    $.ajax({
+        type: 'patch',
+        url: '/todos/' + id + '.json',
+        data: {
+          todo: updatedTodo
+        }
+    }).done(function(data) {
+      _this.$el.toggleClass("done-true");
+    });
   }
 
 });
