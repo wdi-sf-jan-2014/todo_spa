ce4464c966741f64abd330c1474c033ca37db78f
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2014-03-03 20:45:40 -0800

    app is working, all but 1 tests are passing

--- app/assets/javascripts/views/todos/todos_index.js
+++ app/assets/javascripts/views/todos/todos_index.js
@@ -3,15 +3,39 @@ SpaApp.Views.TodosIndex = Backbone.View.extend({
 
   template: HandlebarsTemplates['todos/index'],
 
+  events: {
+    'submit #addTodo':                'add'
+  },
+
   render: function() {
     $(this.el).html(this.template());
 
+    var todoView;
     _.each(this.collection, function (someTodo) {
-      var todoHTML = HandlebarsTemplates['todos/show'](someTodo);
-      this.$el.append(todoHTML);
+      todoView = new SpaApp.Views.TodosShow({ model: someTodo} );
+      this.$el.append(todoView.render().el);
     }, this);
 
     return this;
-  }
+  },
 
+  add: function(event) {
+    event.preventDefault();
+    
+    var newTodo = {
+      title: $("#todo_title").val(),
+      completed: false
+    };
+
+    $.ajax({
+      type: "POST",
+      url: '/todos.json',
+      context: this,
+      data: {todo: newTodo}
+    }).done(function (todo) {
+      var todoView = new SpaApp.Views.TodosShow({ model: todo });
+      this.$el.append(todoView.render().el);
+    });
+  }
 });
+
