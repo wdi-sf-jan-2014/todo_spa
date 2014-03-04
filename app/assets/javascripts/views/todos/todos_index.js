a3d95b3cc076d21407d6ad01dadca06a6025cecd
mbetts7 <mbetts7@gmail.com>
2014-03-03 18:47:31 -0800

    working app with everything in index

--- app/assets/javascripts/views/todos/todos_index.js
+++ app/assets/javascripts/views/todos/todos_index.js
@@ -3,6 +3,45 @@ SpaApp.Views.TodosIndex = Backbone.View.extend({
 
   template: HandlebarsTemplates['todos/index'],
 
+  events: {
+    "submit #addTodo": "addTodo",
+    "click #removeTodo": "removeTodo",
+    "click #todo_completed": "checkTodo"
+  },
+
+  addTodo: function(event) {
+    event.preventDefault();
+    console.log("new todo submitted");
+    var newTodo = { todo: {
+      title: $('#todo_title').val(),
+      completed: false }
+    };
+    // could do context: this then you can do this.$el.append(html)
+    var todo = this;
+    $.ajax({type: "POST", url: "/todos.json", data: newTodo}).done(function(response) {
+      console.log(response);
+      var html = HandlebarsTemplates['todos/show'](response);
+      todo.$el.append(html);
+    });
+  },
+
+  checkTodo: function(event){
+   var updated_todo = {};
+   updated_todo.id = $(event.target).closest('.todo').data().id;
+   updated_todo.completed = event.target.checked;
+   $(event.target).closest('.todo').toggleClass('done-true');
+
+   $.ajax({type: 'patch', url: '/todos/' + updated_todo.id + '.json', data: { todo: updated_todo}});
+  },
+
+  removeTodo: function(event) {
+    event.preventDefault();
+    var id = $(event.target).closest('.todo').data().id;
+    $(event.target).closest('.todo').remove();
+    $.ajax( { type: 'delete', url: "/todos/"+id + ".json" } );
+  },
+
+
   render: function() {
     $(this.el).html(this.template());
 
