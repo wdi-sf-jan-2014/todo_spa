6639e267d1647fcd032f21e0f74869832375c844
alanvp <apvanpelt@yahoo.com>
2014-03-04 10:17:10 -0800

    added delete functionality

--- app/assets/javascripts/views/todos/todos_index.js
+++ app/assets/javascripts/views/todos/todos_index.js
@@ -3,6 +3,50 @@ SpaApp.Views.TodosIndex = Backbone.View.extend({
 
   template: HandlebarsTemplates['todos/index'],
 
+  events: {
+    "submit #addTodo": "addTodo",
+    "click #removeTodo": "removeTodo",
+    "click #todo_completed": "updateTodo"
+  }, 
+
+  addTodo: function(event) {
+    event.preventDefault();
+    var title = $('#todo_title').val();
+    var params = { todo: { title: title } };
+    var _this = this;
+    $.ajax({type: 'post', url: "/todos.json", data: params}).done(function(response){
+      var html = HandlebarsTemplates['todos/show'](response);
+      _this.$el.append(html);
+    });
+  },
+
+  removeTodo: function(event) {
+    event.preventDefault();
+    $(event.target).closest('.todo').remove();
+    var id = $(event.target).closest('.todo').data('id');
+    $.ajax({type: 'delete', url: "/todos/"+id+".json"}).done(function(response){
+    });
+  },
+
+  updateTodo: function(event) {
+    var id = $(event.target).closest('.todo').data('id');
+    if ($(event.target).is(':checked')) {
+      var params = { todo: { completed: true } };
+      $.ajax({type: 'patch', url: "/todos/"+id+".json", data: params}).done(function(response){
+        $(event.target).closest('.todo').toggleClass('done-true');  
+      });
+    }
+    else {
+      var params = { todo: { completed: false } };
+      $.ajax({type: 'patch', url: "/todos/"+id+".json", data: params}).done(function(response){
+        $(event.target).closest('.todo').toggleClass('done-true'); 
+      });  
+    }  
+      
+
+  },
+
+
   render: function() {
     $(this.el).html(this.template());
 
@@ -13,5 +57,8 @@ SpaApp.Views.TodosIndex = Backbone.View.extend({
 
     return this;
   }
+
+ 
+
 
 });
