3e7236406d580de75084a5061e8cf66bf0b9f22e
melodicodyssey <stevedbauer@gmail.com>
2014-03-04 18:44:37 -0800

    added new Router object to initialize method

--- app/assets/javascripts/spa_app.js
+++ app/assets/javascripts/spa_app.js
@@ -6,11 +6,16 @@ window.SpaApp = {
   initialize: function() {
     // this code obviously belongs in a model or collection
     // but, we're not talking about models or collections just yet :)
+
+    var this.router = new SpaApp.Routers.Main;
+    Backbone.history.start({pushState: true});
+    
     $.get("/todos.json").done(function (data) {
       // initialize the index view with the fetched data
       var view = new SpaApp.Views.TodosIndex({ collection: data });
       $('#container').html(view.render().el);
-    });
+    });    
+
   }
 };
 
@@ -20,8 +25,13 @@ $(document).ready(function(){
 
 SpaApp.Routers.Main = Backbone.Router.extend({
   routes: {
+    "": "main",
     "/details/:description": "showDescription",
     "*other": "defaultRoute"
+  },
+
+  main: function() {
+
   },
 
   showDescription: function(desc) {
