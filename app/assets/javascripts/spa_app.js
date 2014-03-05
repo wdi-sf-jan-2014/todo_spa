e8904d2404b847b7c0cd4156dcb6c14c184c81f4
smarsh25 <sjm.atl@gmail.com>
2014-03-04 16:55:22 -0800

    successful refactoring, more complete migration to / use of backbone router

--- app/assets/javascripts/spa_app.js
+++ app/assets/javascripts/spa_app.js
@@ -7,17 +7,6 @@ window.SpaApp = {
   initialize: function() {
     this.router = new this.Routers.Main();
     Backbone.history.start({pushState: true});
-    this.makeIndex();
-  },
-
-  makeIndex: function() {
-    // this code obviously belongs in a model or collection
-    // but, we're not talking about models or collections just yet :)
-    $.get("/todos.json").done(function (data) {
-      // initialize the index view with the fetched data
-      var view = new SpaApp.Views.TodosIndex({ collection: data });
-      $('#container').html(view.render().el);
-    });    
   }
 };
 
--- app/assets/javascripts/todos_router.js
+++ app/assets/javascripts/todos_router.js
@@ -1,3 +1,16 @@
 SpaApp.Routers.Main = Backbone.Router.extend({
+  routes: {
+    "": "main"
+  },
 
+  main: function() {
+    // this code obviously belongs in a model or collection
+    // but, we're not talking about models or collections just yet :)
+    $.get("/todos.json"
+    ).done(function (data) {
+      // initialize the index view with the fetched data
+      var view = new SpaApp.Views.TodosIndex({ collection: data });
+      $('#container').html(view.render().el);
+    });
+  }
 });
